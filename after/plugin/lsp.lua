f3e39dd2ad2c65cc58b9ad5970993fced74c5716
juan <jouglardjuan@gmail.com>
2024-08-01 12:43:32 -0300

    python support

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -9,9 +9,10 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
+
 -- Replace the language servers listed here
 -- with the ones you want to install
-ensure_installed = {'tsserver', 'rust_analyzer'},
+ensure_installed = {'tsserver', 'rust_analyzer', 'pyright'},
 handlers = {
   function(server_name)
     require('lspconfig')[server_name].setup({})
@@ -23,21 +24,24 @@ local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
-window = {
-  completion = cmp.config.window.bordered(),
-  documentation = cmp.config.window.bordered(),
-},
-mapping = cmp.mapping.preset.insert({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
-}),
-snippet = {
-  expand = function(args)
-    require('luasnip').lsp_expand(args.body)
-  end,
-},
+    sources = {
+        {name = 'nvim_lsp'},
+    },
+    window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+      ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+      ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+      ["<C-Space>"] = cmp.mapping.complete(),
+    }),
+    snippet = {
+      expand = function(args)
+        require('luasnip').lsp_expand(args.body)
+      end,
+    },
 })
 
 
